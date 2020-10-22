7c856c49ed5f0049b37427234cf9bef56b4500eb
tomakor549 <venetor7@gmail.com>
2020-10-23 01:53:43 +0200

    ustawienie poprawnie toolbara i eksperymenty na klasach

--- app/src/main/java/com/example/inzynierka/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/example/inzynierka/ui/article/ArticleFragment.kt
@@ -1,6 +1,28 @@
 package com.example.inzynierka.ui.article
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.inzynierka.R
 
 class ArticleFragment : Fragment() {
+
+    private lateinit var articleViewModel: ArticleViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        articleViewModel = ViewModelProvider(this).get(ArticleViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_article, container, false)
+        val textView: TextView = root.findViewById(R.id.txt_article)
+        articleViewModel.text.observe(viewLifecycleOwner, Observer { textView.text = it })
+        return root
+    }
 }
--- app/src/main/java/com/example/inzynierka/ui/article/ArticleViewModel.kt
+++ app/src/main/java/com/example/inzynierka/ui/article/ArticleViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.inzynierka.ui.article
 
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ArticleViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "Pusty element"
+    }
+    val text: LiveData<String> = _text
 }
