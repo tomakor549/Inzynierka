e2512f569ac63aea33708dd5963c1ef18c511415
tomakor549 <venetor7@gmail.com>
2020-11-17 15:48:13 +0100

    new activity article

---
+++ app/src/main/java/com/example/inzynierka/ui/article/ArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inzynierka.ui.article
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inzynierka.R
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
--- app/src/main/java/com/example/inzynierka/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/example/inzynierka/ui/article/ArticleFragment.kt
@@ -1,14 +1,17 @@
 package com.example.inzynierka.ui.article
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.inzynierka.R
+import com.example.inzynierka.StartActivity
+import kotlinx.android.synthetic.main.fragment_article.*
+import kotlinx.android.synthetic.main.fragment_article.view.*
 
 class ArticleFragment : Fragment() {
 
@@ -22,9 +25,13 @@ class ArticleFragment : Fragment() {
         articleViewModel = ViewModelProvider(this).get(ArticleViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_article, container, false)
 
-
+        root.article_city.setOnClickListener{
+            val intent = Intent(activity, ArticleActivity::class.java)
+            startActivity(intent)
+        }
         //val textView: TextView = root.findViewById(R.id.txt_article)
         //articleViewModel.text.observe(viewLifecycleOwner, Observer { textView.text = it })
         return root
     }
+
 }
